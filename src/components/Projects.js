d8fac8e8b8e6424d711915adc3d5c33e2860e51e
AaronW <aaronwang96@hotmail.com>
2023-07-20 01:41:57 -0700

    finished Project Section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,44 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
 
 export const Projects = () => {
 
   const projects = [
     {
-      title: "ChatterApp",
-      description: "Design & Development",
+      title: "Typescript Demo",
+      description: "Fullstack PERN with Typescript and Redux",
       imgUrl: projImg1,
+      websiteUrl: "https://github.com/Aaronw7/ts-demo",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "BudgetPal",
+      description: "Frontend React MUI",
       imgUrl: projImg2,
+      websiteUrl: "https://github.com/Aaronw7/budget-pal",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Minting Web3",
+      description: "Frontend React with Basic Solidity Smart Contract",
       imgUrl: projImg3,
-    }
+      websiteUrl: "https://github.com/Aaronw7/mint-web3",
+    },
+    {
+      title: "Windfall SaaS",
+      description: "Fullstack React with Flask",
+      imgUrl: projImg4,
+      websiteUrl: "https://github.com/Aaronw7/windfall",
+    },
+    {
+      title: "ChatterApp",
+      description: "Fullstack MERN with WebSockets",
+      imgUrl: projImg5,
+      websiteUrl: "http://chatterapp.net",
+    },
   ];
 
   return (
@@ -31,41 +48,18 @@ export const Projects = () => {
           <Col size={12}>
             <h2>Projects</h2>
             <p>Feel free to check out the projects I have listed here!</p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">ChatterApp</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">RoboPunks</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">ChatterApp</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content id="slideInUp">
-                <Tab.Pane eventKey="first">
-                  <Row>
-                    {/* {
-                      projects.map((project, index) => {
-                        return (
-                          <ProjectCard
-                            key={index}
-                            {...project}
-                            />
-                        )
-                      })
-                    } */}
-                  </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                </Tab.Pane>
-                <Tab.Pane eventKey="third">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                </Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
+            <Row>
+              {
+                projects.map((project, index) => {
+                  return (
+                    <ProjectCard
+                      key={index}
+                      {...project}
+                      />
+                  )
+                })
+              }
+            </Row>
           </Col>
         </Row>
       </Container>
